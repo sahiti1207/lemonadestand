27c17c1a5f3d7ab00ede8c0f64c571d2de11fd99
sahiti1207 <smiley.sahithi@gmail.com>
2023-11-08 18:35:14 -0600

    Initial commit

---
+++ src/main/java/com/example/lemonadestand/LemonadestandApplication.java
@@ -0,0 +1,13 @@
+package com.example.lemonadestand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonadestandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemonadestandApplication.class, args);
+	}
+
+}
